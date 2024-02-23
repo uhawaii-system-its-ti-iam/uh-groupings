b21825a13fd6d1c3e2d87217faf6334de61ed8f3
michho8 <mh888@hawaii.edu>
2024-02-22 18:45:51 -1000

    Replicate all basic components

--- ui/src/app/feedback/page.tsx
+++ ui/src/app/feedback/page.tsx
@@ -9,6 +9,8 @@ import {Input} from "@/components/ui/input";
 //   FormMessage,
 // } from "@/components/ui/form"
 import {Label} from "@/components/ui/label";
+import {blue} from "next/dist/lib/picocolors";
+import {Button} from "@/components/ui/button";
 
 
 const Feedback = () => (
@@ -25,33 +27,38 @@ const Feedback = () => (
       //   </FormItem>
       // )}></FormField>
     <>
-    <div className="container flex">
-      <div className="columns-1">
-        <h1 className="text-md-left">Feedback</h1>
-        <p>Helps us to understand where improvements are needed. Please let us know.</p>
+    <div className="container grid grid-cols-12 pt-5 pb-4">
+      <div className="col-span-5 pt-5">
+        <h1 className="text-md-left text-3xl font-bold">Feedback</h1>
+        <p className="text-md-left text-xl mt-3">Helps us to understand where improvements are needed. Please let us know.</p>
       </div>
-      <div className="columns-1">
-        <div className="row-auto">
-          <Label htmlFor="feedback-type">Feedback Type:*</Label>
-          <select id="feedback-type" className="bg-gray-50 border">
-            <option selected>Choose a type</option>
-            <option value="General">General</option>
+      <div className="col-span-7 pl-5 pt-5">
+        <div className="pb-3">
+          <Label htmlFor="input-type" className="text-base">
+            Feedback Type:
+            <span className="required-asterisk">*</span>
+          </Label>
+          <select id="input-type" className="w-full px-3 py-1 border border-gray-300 rounded-md focus:accent-blue-500 focus:border-blue-500">
+            <option value="General" selected>General</option>
             <option value="Problem">Problem</option>
             <option value="Feature">Feature</option>
             <option value="Question">Question</option>
           </select>
         </div>
-        <div className="row-auto">
-          <Label htmlFor="name">Your Name (Optional):</Label>
+        <div className="pt-5 pb-3">
+          <Label htmlFor="name" className="text-base">Your Name (Optional):</Label>
           <Input id="name" placeholder="John Doe"></Input>
         </div>
-        <div className="row-auto">
-          <Label type="email" htmlFor="email">Email Address*</Label>
+        <div className="pt-5 pb-3">
+          <Label htmlFor="email" className="text-base">Email Address*</Label>
           <Input id="email" placeholder="user@email.com"></Input>
         </div>
-        <div className="row-auto">
-          <Label htmlFor="comments">Your Feedback*</Label>
-          <Input id="comments"></Input>
+        <div className="pt-5 pb-3">
+          <Label htmlFor="comments" className="text-base">Your Feedback*</Label>
+          <textarea id="comments" className="w-full px-3 py-1 border border-gray-300 rounded-md focus:accent-blue-500 focus:border-blue-500"></textarea>
+        </div>
+        <div className="pt-5">
+          <Button>Submit</Button>
         </div>
       </div>
     </div>
