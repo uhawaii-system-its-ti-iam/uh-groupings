dd5853c8be057de8c9cbf6c3c5883010c15bad7c
michho8 <mh888@hawaii.edu>
2024-02-23 12:06:31 -1000

    Put all fields into a Form tag

--- ui/src/app/feedback/page.tsx
+++ ui/src/app/feedback/page.tsx
@@ -1,31 +1,112 @@
+"use client"
 import React from 'react';
 import {Input} from "@/components/ui/input";
-// import {
-//   FormControl,
-//   FormDescription,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import {Label} from "@/components/ui/label";
-import {blue} from "next/dist/lib/picocolors";
-import {Button} from "@/components/ui/button";
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 
+const FeedbackForm = () => {
+  const formSchema = z.object({
+    type: z.string(),
+    name: z.string(),
+    email: z.string().email(),
+    feedback: z.string().min(1),
+  })
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "username@email.com",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <>
+        <div className="container grid grid-cols-12 pt-5 pb-4">
+          <div className="col-span-5 pt-5">
+            <h1 className="text-md-left text-3xl font-bold">Feedback</h1>
+            <p className="text-md-left text-xl mt-3">Helps us to understand where improvements are needed. Please let us know.</p>
+          </div>
+          <div className="col-span-7 pl-5 pt-5">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField control={form.control} name="type"
+                render={({ field }) => (
+                  <FormItem className="pb-3">
+                    <FormLabel htmlFor="type" className="text-base">Feedback Type:*</FormLabel>
+                    <FormControl>
+                      <select id="type" className="w-full px-3 py-1 border border-gray-300 rounded-md focus:accent-blue-500 focus:border-blue-500" {...field}>
+                        <option value="General" selected>General</option>
+                        <option value="Problem">Problem</option>
+                        <option value="Feature">Feature</option>
+                        <option value="Question">Question</option>
+                      </select>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField control={form.control} name="name"
+                render={({ field }) => (
+                  <FormItem className="pt-5 pb-3">
+                    <FormLabel htmlFor="name" className="text-base">Your Name (Optional):</FormLabel>
+                    <FormControl>
+                      <Input id="name"  placeholder="John Doe" {...field} />
+                    </FormControl>
+                  <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField control={form.control} name="email"
+                render={({ field }) => (
+                  <FormItem className="pt-5 pb-3">
+                    <FormLabel htmlFor="email" className="text-base">Email Address*</FormLabel>
+                    <FormControl>
+                      <Input id="email"  placeholder="Enter your email here" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField control={form.control} name="feedback"
+                render={({ field }) => (
+                  <FormItem className="pt-5 pb-3">
+                    <FormLabel htmlFor="feedback" className="text-base">Your Feedback*</FormLabel>
+                    <FormControl>
+                      <textarea id="feedback" className="w-full px-3 py-1 border border-gray-300 rounded-md focus:accent-blue-500 focus:border-blue-500" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </div>
+        </div>
+      </>
+    </Form>
+  )
+}
+
+export default FeedbackForm;
 
 const Feedback = () => (
-      // <FormField name="name" render={() => (
-      //   <FormItem>
-      //     <FormLabel>Username</FormLabel>
-      //     <FormControl>
-      //       <Input placeholder="shadcn" />
-      //     </FormControl>
-      //     <FormDescription>
-      //       This is your public display name.
-      //     </FormDescription>
-      //     <FormMessage />
-      //   </FormItem>
-      // )}></FormField>
     <>
     <div className="container grid grid-cols-12 pt-5 pb-4">
       <div className="col-span-5 pt-5">
@@ -65,4 +146,4 @@ const Feedback = () => (
   </>
 );
 
-export default Feedback;
+// export default Feedback;
