1980779488bc3bb22faf61945dff85cdf8aedfb3
Jordan Wong <42422209+JorWo@users.noreply.github.com>
2024-03-07 11:41:19 -1000

    Add layer of security for invalid tickets from CAS (#6)

--- ui/tests/access/Saml11Validator.test.ts
+++ ui/tests/access/Saml11Validator.test.ts
@@ -3,6 +3,7 @@ import User, { AnonymousUser } from '@/access/User';
 
 const testUser: User = JSON.parse(process.env.TEST_USER_A as string);
 const xmlSoapResponse = process.env.XML_SOAP_RESPONSE as string;
+const xmlSoapResponseRequestDenied = process.env.XML_SOAP_RESPONSE_REQUEST_DENIED as string;
 
 describe('Saml11Validator', () => {
 
@@ -15,6 +16,11 @@ describe('Saml11Validator', () => {
 
         it('should return an AnonymousUser on error', async () => {
             fetchMock.mockAbort();
+            expect(await validateTicket('ticket')).toEqual(AnonymousUser);
+        });
+
+        it('should return an AnonymousUser on invalid ticket', async () => {
+            fetchMock.mockResponse(xmlSoapResponseRequestDenied);
             expect(await validateTicket('ticket')).toEqual(AnonymousUser);
         });
 
