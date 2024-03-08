58759deba3f01d3526cda108c40ec5e2585f6bf4
Jordan Wong <jordanw4@hawaii.edu>
2024-03-08 13:33:49 -1000

    Create headers for /admin /groupings /memberships

---
+++ ui/tests/app/admin/page.test.tsx
@@ -0,0 +1,20 @@
+import Admin from '@/app/admin/page';
+import { render, screen } from '@testing-library/react';
+
+describe('Admin', () => {
+
+    it('should render the Admin page with the appropriate header and tabs', () => {
+        render(<Admin />);
+        expect(screen.getByRole('main')).toBeInTheDocument();
+
+        expect(screen.getByRole('heading', { name: 'UH Groupings Administration' })).toBeInTheDocument();
+        expect(screen.getByText('Search for and manage any grouping on behalf of its owner. ' + 
+            'Manage the list of UH Groupings administrators.')).toBeInTheDocument();
+
+        expect(screen.getByRole('tablist')).toBeInTheDocument();
+        expect(screen.getByRole('tab', { name: 'Manage Groupings' })).toBeInTheDocument();
+        expect(screen.getByRole('tab', { name: 'Manage Admins' })).toBeInTheDocument();
+        expect(screen.getByRole('tab', { name: 'Manage Person' })).toBeInTheDocument();
+    });
+
+});
