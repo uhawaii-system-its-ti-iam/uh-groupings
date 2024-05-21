587dd819c6f3bf7e8d9e49993476fd5bbe47805d
Hok Wai Chan <hokwai@hawaii.edu>
2024-05-20 23:43:00 -1000

    Make Components for the About Page

---
+++ ui/src/app/about/_components/GeneralInfoSection.tsx
@@ -0,0 +1,55 @@
+const GeneralInfoSection = () => {
+    const generalInfoItems = [
+        {
+            question: 'How do I request a new grouping?',
+            answer: (
+                <a className="text-link-color hover:underline hover:text-link-hover-color"
+                    href="https://uhawaii.atlassian.net/wiki/spaces/UHIAM/pages/13402308/UH+Groupings+Request+Form"
+                    aria-label="A request form is available">
+                        A request form is available.
+                </a>
+            )
+        },
+        {
+            question: 'What is the Include members list?',
+            answer: 'A grouping\'s Include is the portion of the grouping membership that is manually updated. It may be empty.'
+        },
+        {
+            question: 'Exactly what is a grouping?',
+            answer: (
+                <>
+                    <a className="text-link-color hover:underline hover:text-link-hover-color"
+                        href="https://uhawaii.atlassian.net/wiki/spaces/UHIAM/pages/13403213/UH+Groupings"
+                        aria-label="General information about groupings is available">
+                        General information about groupings is available.
+                    </a> {' '}
+                    A grouping&apos;s components include the Basis, Include, and Exclude lists.
+                </>
+            )
+        },
+        {
+            question: 'What is the Exclude members list?',
+            answer: 'A grouping\'s Exclude overrides automatic and manual membership by explicitly not including anyone listed here. It may be empty.'
+        },
+        {
+            question: 'What is the Basis?',
+            answer: 'A grouping\'s Basis is the portion of the grouping membership that is automatically updated. It may be empty.'
+        }
+    ];
+
+    return (
+        <div className="container pt-10 pb-10">
+            <h2 className="text-center text-xl font-bold text-text-color pb-7">GENERAL INFORMATION</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {generalInfoItems.map((generalInfoItem, index) => (
+                    <div key={index}>
+                        <h3 className="text-text-color text-lg pb-1">{generalInfoItem.question}</h3>
+                        <p>{generalInfoItem.answer}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default GeneralInfoSection;
---
+++ ui/src/app/about/_components/WhatHappensIfSection.tsx
@@ -0,0 +1,34 @@
+const WhatHappensIfSection = () => {
+    const whatHappensIfItems = [
+        {
+            question: 'I was an owner of just one grouping and someone (me or another owner) deleted my ownership while I was still logged in.',
+            answer: 'You will still see the Groupings menu option, but you will get an error message when you click on it. Next time you log in, the Groupings menu option will no longer appear, assuming that you don\'t reacquire the ownership of a grouping before then.'
+        },
+        {
+            question: 'I was not an owner of any groupings and someone made me an owner while I was still logged in.',
+            answer: 'You will have to log out and then log back in again to see the Groupings menu option.'
+        },
+        {
+            question: 'I was an admin and someone (me or another admin) deleted my admin role while I was still logged in.',
+            answer: 'You will still see the Admin menu option, but you will get an error message when you click on it. Next time you log in, the Admin menu option will no longer appear, assuming that you don\'t reacquire the Admin role before then.'
+        }
+    ];
+
+    return (
+        <div className="bg-seafoam pt-5">
+            <div className="container pb-10">
+                <h2 className="text-center text-xl text-text-color font-bold pb-7 pt-5">WHAT HAPPENS IF</h2>
+                <div className="grid gap-8">
+                    {whatHappensIfItems.map((item, index) => (
+                        <div key={index}>
+                            <h3 className="text-text-color text-lg pb-2">Q: {item.question}</h3>
+                            <p>A: {item.answer}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WhatHappensIfSection;
