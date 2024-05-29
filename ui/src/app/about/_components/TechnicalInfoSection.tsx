62b78d38066506e369daa5f85665fdda539f2cc4
Hok Wai Chan <77360958+hokwaichan@users.noreply.github.com>
2024-05-28 21:56:32 -1000

    Make Components for the About Page (#17)

---
+++ ui/src/app/about/_components/TechnicalInfoSection.tsx
@@ -0,0 +1,47 @@
+const TechnicalInfoSection = () => {
+    const technicalInfoItems = [
+        { name: 'React.js', url: 'https://react.dev/learn', description: '(Quickstart)' },
+        { name: 'shadcn/ui', url: 'https://ui.shadcn.com/docs', description: '(Guide)' },
+        { name: 'Jest', url: 'https://jestjs.io/docs/getting-started', description: '(Introduction)' },
+        { name: 'Next.js', url: 'https://nextjs.org/docs', description: '(Introduction)' },
+        { name: 'Tanstack Table', url: 'https://tanstack.com/table/v8/docs/introduction', description: '(Introduction)' },
+        { name: 'React Testing Library', url: 'https://testing-library.com/', description: '(Documentation)' },
+        { name: 'Typescript', url: 'https://www.typescriptlang.org/docs/', description: '(Guide)' },
+        { name: 'Tanstack Query', url: 'https://tanstack.com/query/latest/docs/framework/react/overview', description: '(Guide)' },
+        { name: '', url: '', description: ''},
+        { name: 'Tailwind CSS', url: 'https://v2.tailwindcss.com/docs', description: '(Guide)' },
+        { name: 'Iron Session', url: 'https://github.com/vvo/iron-session', description: '(GitHub)' }
+    ];
+
+    return (
+        <div className="container pb-10 pt-6">
+            <h2 className="text-center text-text-color text-xl font-bold pb-7">TECHNICAL INFORMATION</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div className="col-span-1">
+                    <h3 className="text-text-color text-lg pb-2">Resources</h3>
+                    <p className="pb-3">Source code is available on <a
+                        className="text-link-color hover:underline hover:text-link-hover-color"
+                        href="https://github.com/uhawaii-system-its-ti-iam/uh-groupings">GitHub</a>
+                    </p>
+                </div>
+                <div className="col-span-3">
+                    <h3 className="text-text-color text-lg pb-2">Technologies</h3>
+                    <div className="grid grid-cols-1 md:grid-cols-3 md:gap-4 gap-0">
+                        {technicalInfoItems.map((item, index) => (
+                            <div key={index} >
+                                <p>
+                                    {item.name}&nbsp;
+                                    <a className="text-link-color hover:underline hover:text-link-hover-color" href={item.url} aria-label={`Link to ${item.name} ${item.description}`}>
+                                        {item.description}
+                                    </a>
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TechnicalInfoSection;
---
+++ ui/tests/app/about/_components/WhatHappensIfSection.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import WhatHappensIfSection from '@/app/about/_components/WhatHappensIfSection';
+
+describe('WhatHappensIfSection', () => {
+    it('should render with question and answer', () => {
+        render(<WhatHappensIfSection />);
+
+        expect(screen.getByText('Q: I was an owner of just one grouping and someone (me or another owner) deleted my ownership while I was still logged in.')).toBeInTheDocument();
+        expect(screen.getByText('A: You will still see the Groupings menu option, but you will get an error message when you click on it. Next time you log in, the Groupings menu option will no longer appear, assuming that you don\'t reacquire the ownership of a grouping before then.')).toBeInTheDocument();
+
+        expect(screen.getByText('Q: I was not an owner of any groupings and someone made me an owner while I was still logged in.')).toBeInTheDocument();
+        expect(screen.getByText('A: You will have to log out and then log back in again to see the Groupings menu option.')).toBeInTheDocument();
+
+        expect(screen.getByText('Q: I was an admin and someone (me or another admin) deleted my admin role while I was still logged in.')).toBeInTheDocument();
+        expect(screen.getByText('A: You will still see the Admin menu option, but you will get an error message when you click on it. Next time you log in, the Admin menu option will no longer appear, assuming that you don\'t reacquire the Admin role before then.')).toBeInTheDocument();
+    });
+});
