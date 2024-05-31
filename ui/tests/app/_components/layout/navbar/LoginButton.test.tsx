d9906ae0d3535affc6f7112bea2dfa52c9a34788
Hok Wai Chan <77360958+hokwaichan@users.noreply.github.com>
2024-05-31 09:51:40 -1000

    Move the .tsx files in /components/layout and /components/modal folders into /_component folders within /app (#22)

--- ui/tests/app/_components/layout/navbar/LoginButton.test.tsx
+++ ui/tests/app/_components/layout/navbar/LoginButton.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Login from '@/components/layout/navbar/LoginButton';
+import Login from '@/app/_components/layout/navbar/LoginButton';
 import { redirect } from 'next/navigation';
 import User, { AnonymousUser } from '@/access/User';
 import Role from '@/access/Role';
