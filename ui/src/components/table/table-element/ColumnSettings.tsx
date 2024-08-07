3416e2f68ad97a7e56fa5a8e10e97a2ee4de009b
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2024-08-06 21:53:29 -1000

    update

--- ui/src/components/table/table-element/ColumnSettings.tsx
+++ ui/src/components/table/table-element/ColumnSettings.tsx
@@ -1,17 +1,18 @@
 'use client';
-import { Label } from '@/components/ui/label';
-import { Switch } from '@/components/ui/switch';
-import { useState } from 'react';
-import { Button } from '@/components/ui/button';
+import {Label} from '@/components/ui/label';
+import {Switch} from '@/components/ui/switch';
+import {useState} from 'react';
+import {Button} from '@/components/ui/button';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {
     DropdownMenu,
     DropdownMenuTrigger,
     DropdownMenuContent,
-    DropdownMenuRadioGroup,
-    DropdownMenuRadioItem
+    DropdownMenuItem
 } from '@/components/ui/dropdown-menu';
+import {faSliders} from "@fortawesome/free-solid-svg-icons";
 
-const ColumnSettings = ({ table }) => {
+const ColumnSettings = ({table}) => {
     const [description, setDescription] = useState(true);
     const [groupingPath, setGroupingPath] = useState(true);
 
@@ -35,27 +36,42 @@ const ColumnSettings = ({ table }) => {
         <div>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                    <Button variant="outline" className="border border-gray-300 hover:bg-transparent">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-text-color">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"/>
-                        </svg>
+                    <Button
+                        variant="outline"
+                        className="border border-gray-300 hover:bg-transparent"
+                        aria-label="Column settings"
+                    >
+                        <FontAwesomeIcon
+                            icon={faSliders}
+                            className="w-5 h-5 text-text-color"
+                        />
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
-                    <DropdownMenuRadioGroup>
-                        <DropdownMenuRadioItem value="description" className="px-2">
-                            <div className="flex items-center space-x-2">
-                                <Switch id="description" checked={description} onCheckedChange={toggleDescription} className="data-[state=checked]:bg-uh-teal"/>
-                                <Label htmlFor="description">Description</Label>
-                            </div>
-                        </DropdownMenuRadioItem>
-                        <DropdownMenuRadioItem value="grouping path" className="px-2">
-                            <div className="flex items-center space-x-2">
-                                <Switch id="grouping-path" checked={groupingPath} onCheckedChange={toggleGroupingPath}  className="data-[state=checked]:bg-uh-teal"/>
-                                <Label htmlFor="grouping-path">Grouping Path</Label>
-                            </div>
-                        </DropdownMenuRadioItem>
-                    </DropdownMenuRadioGroup>
+                    <DropdownMenuItem>
+                        <div className="flex items-center space-x-2">
+                            <Switch
+                                id="description"
+                                checked={description}
+                                onCheckedChange={toggleDescription}
+                                className="data-[state=checked]:bg-uh-teal"
+                                aria-label="Toggle Description column"
+                            />
+                            <Label htmlFor="description">Description</Label>
+                        </div>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <div className="flex items-center space-x-2">
+                            <Switch
+                                id="grouping-path"
+                                checked={groupingPath}
+                                onCheckedChange={toggleGroupingPath}
+                                className="data-[state=checked]:bg-uh-teal"
+                                aria-label="Toggle Grouping Path column"
+                            />
+                            <Label htmlFor="grouping-path">Grouping Path</Label>
+                        </div>
+                    </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
         </div>
