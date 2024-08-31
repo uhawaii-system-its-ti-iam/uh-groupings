1002aa875abe14502c4240c268262bf368de9b91
Prayag Das <prayag.j.das2001@gmail.com>
2024-08-30 18:05:33 -1000

    Modified modal design to resemble the Angular project more closely.

--- ui/src/components/table/adminTable/table-element/RemoveAdminsDialog.tsx
+++ ui/src/components/table/adminTable/table-element/RemoveAdminsDialog.tsx
@@ -1,40 +1,48 @@
-import {Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger} from '@/components/ui/dialog';
+import {
+    AlertDialog, 
+    AlertDialogContent, 
+    AlertDialogDescription,
+    AlertDialogFooter, 
+    AlertDialogHeader, 
+    AlertDialogTitle, 
+    AlertDialogTrigger
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
 import { Alert, AlertDescription } from '@/components/ui/alert';
 import {Label} from '@/components/ui/label';
-import {Button} from '@/components/ui/button';
 import { Trash2Icon } from 'lucide-react';
 
 const RemoveAdminsDialog = () => {
 
   return (
-      <Dialog>
-          <DialogTrigger asChild>
+      <AlertDialog>
+          <AlertDialogTrigger asChild>
               <Trash2Icon
                 className="h-4 w-4 text-red-600"
               />
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                  <DialogTitle className="text-[1.5rem] text-text-color">Remove Member</DialogTitle>
-                  <hr/>
-                  <DialogDescription>
+          </AlertDialogTrigger>
+          <AlertDialogContent className="sm:max-w-[500px]">
+              <AlertDialogHeader>
+                  <AlertDialogTitle className="text-[1.4rem] text-text-color">Remove Member</AlertDialogTitle>
+                  {/*<hr/>*/}
+                  <AlertDialogDescription>
                       You are about to remove the following member from the admins list.
-                  </DialogDescription>
-              </DialogHeader>
+                  </AlertDialogDescription>
+              </AlertDialogHeader>
               <div className="grid grid-cols-2">
-                  <div className="col-first grid py-4">
-                      <div className="grid grid-cols-3 items-center ">
-                          <Label htmlFor="name" className="font-bold text-xs text-left whitespace-nowrap">
+                  <div className="grid">
+                      <div className="grid grid-cols-3 items-center py-1 px-4">
+                          <Label htmlFor="name" className="font-bold text-s text-left whitespace-nowrap">
                               NAME:
                           </Label>
                       </div>
-                      <div className="grid grid-cols-4 items-center">
-                          <Label htmlFor="name" className="font-bold text-xs text-left whitespace-nowrap">
+                      <div className="grid grid-cols-3 items-center py-1 px-4">
+                          <Label htmlFor="name" className="font-bold text-s text-left whitespace-nowrap">
                               UH USERNAME:
                           </Label>
                       </div>
-                      <div className="grid grid-cols-4 items-center">
-                          <Label htmlFor="name" className="font-bold text-xs text-left whitespace-nowrap">
+                      <div className="grid grid-cols-3 items-center py-1 px-4">
+                          <Label htmlFor="name" className="font-bold text-s text-left whitespace-nowrap">
                               UH USER ID:
                           </Label>
                       </div>
@@ -42,47 +50,49 @@ const RemoveAdminsDialog = () => {
 
                   {/*second column*/}
 
-                  <div className="col-first grid py-4">
+                  <div className="grid">
                       <div className="grid grid-cols-3 items-center">
-                          <Label htmlFor="name" className="text-xs text-left whitespace-nowrap">
+                          <Label htmlFor="name" className="text-s text-left whitespace-nowrap">
                             NAME
                           </Label>
                       </div>
                       <div className="grid grid-cols-4 items-center">
-                          <Label htmlFor="name" className="text-xs text-left whitespace-nowrap">
+                          <Label htmlFor="name" className="text-s text-left whitespace-nowrap">
                               UH USERNAME
                           </Label>
                       </div>
                       <div className="grid grid-cols-4 items-center">
-                          <Label htmlFor="name" className="text-xs text-left whitespace-nowrap">
+                          <Label htmlFor="name" className="text-s text-left whitespace-nowrap">
                               UH USER ID
                           </Label>
                       </div>
                   </div>
               </div>
               <div>
-                  <DialogDescription>
+                  <AlertDialogDescription>
                       Are you sure you want to remove <span className="font-bold text-text-color">NAME</span> from the admins list?
-                  </DialogDescription>
+                  </AlertDialogDescription>
               </div>
-              <Alert>
-                  <AlertDescription>
-                      Membership changes made
-                      may not take effect immediately.
-                      Usually, 3-5 minutes should be
-                      anticipated. In extreme cases changes
-                      may take several hours to be fully
-                      processed, depending on the number
-                      of members and the synchronization
-                      destination.
-                  </AlertDescription>
-              </Alert>
-              <DialogFooter>
+              <div className="px-3">
+                  <Alert className="bg-yellow-100 border border-yellow-200 mb-2">
+                      <AlertDescription>
+                        Membership changes made
+                        may not take effect immediately.
+                        Usually, 3-5 minutes should be
+                        anticipated. In extreme cases changes
+                        may take several hours to be fully
+                        processed, depending on the number
+                        of members and the synchronization
+                        destination.
+                      </AlertDescription>
+                  </Alert>
+              </div>
+              <AlertDialogFooter>
                   <Button>Yes</Button>
                   <Button variant="secondary">Cancel</Button>
-              </DialogFooter>
-          </DialogContent>
-      </Dialog>
+              </AlertDialogFooter>
+          </AlertDialogContent>
+      </AlertDialog>
   );
 };
 
