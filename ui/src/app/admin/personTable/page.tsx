96d3f41e937da62b946add6aa932e1ce611e90ce
Michelle Ho <miho@fhb.com>
2024-09-03 18:00:19 -1000

    Add TailwindCSS table with some mocked data

--- ui/src/app/admin/personTable/page.tsx
+++ ui/src/app/admin/personTable/page.tsx
@@ -37,13 +37,74 @@ const PersonTable = () => {
                     </label>
                     <button
                         type="button"
-                        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4
+                            focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600
+                            dark:hover:bg-red-700 dark:focus:ring-red-900"
                     >
                         Red
                     </button>
                 </div>
             </div>
-            <div>table?</div>
+            <div>
+                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">
+                                    Grouping
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Owner?
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Basis?
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Include?
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Exclude?
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Remove
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="even:dark:bg-gray-900 even:bg-gray-50 odd:dark:bg-gray-800 border-b dark:border-gray-700">
+                                <th
+                                    scope="row"
+                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                >
+                                    JTTEST-L
+                                </th>
+                                <td className="px-6 py-4">Yes</td>
+                                <td className="px-6 py-4">No</td>
+                                <td className="px-6 py-4">Yes</td>
+                                <td className="px-6 py-4">No</td>
+                                <td className="px-6 py-4">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                            <tr className="even:dark:bg-gray-900 even:bg-gray-50 odd:dark:bg-gray-800 border-b dark:border-gray-700">
+                                <th
+                                    scope="row"
+                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                >
+                                    acer-cc-ics
+                                </th>
+                                <td className="px-6 py-4">No</td>
+                                <td className="px-6 py-4">No</td>
+                                <td className="px-6 py-4">No</td>
+                                <td className="px-6 py-4">Yes</td>
+                                <td className="px-6 py-4">
+                                    <input type="checkbox" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
