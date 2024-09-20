cd24b3f1c9a3b3407281683fa7ae475b8ff078d7
Michelle Ho <89873670+michho8@users.noreply.github.com>
2024-09-19 16:07:37 -1000

    Create the ApiErrorModal (#80)

---
+++ ui/src/components/modal/api-error-modal.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import {
+    AlertDialog,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogTitle,
+    AlertDialogFooter,
+    AlertDialogAction,
+    AlertDialogCancel
+} from '@/components/ui/alert-dialog';
+import Link from 'next/link';
+import { useState } from 'react';
+
+const ApiErrorModal = ({ open }: { open: boolean }) => {
+    const [openApiErrorModal, setOpenApiErrorModal] = useState(open);
+
+    /**
+     * Closes the modal.
+     */
+    const close = () => {
+        setOpenApiErrorModal(false);
+    };
+    return (
+        <AlertDialog open={openApiErrorModal}>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Error</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        There was an unexpected communications problem. Please use your browser to refresh the page and
+                        try again.
+                        <br />
+                        <br />
+                        If the error persists please refer to our
+                        <Link className="text-text-color" href={'/feedback'} onClick={() => close()}>
+                            {' '}
+                            feedback page.
+                        </Link>
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel onClick={() => close()}>OK</AlertDialogCancel>
+                    <Link href={'/feedback'}>
+                        <AlertDialogAction onClick={() => close()}>Feedback</AlertDialogAction>
+                    </Link>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+};
+
+export default ApiErrorModal;
