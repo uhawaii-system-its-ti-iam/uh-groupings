4609709f945dd37187fbe8e38a795c351dc63311
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2024-10-31 13:15:54 -1000

    update

--- ui/src/app/memberships/_components/memberships-optIn-cell.tsx
+++ ui/src/app/memberships/_components/memberships-optIn-cell.tsx
@@ -2,17 +2,32 @@ import { Button } from '@/components/ui/button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { optIn } from '@/lib/actions';
+import { useState } from 'react';
 
-const MembershipsOptInCell = ({ groupingPath }: {groupingPath: string }) => {
+const MembershipsOptInCell = ({ groupingPath }: { groupingPath: string }) => {
+    const [loading, setLoading] = useState(false);
     const handleOptOut = async () => {
-        await optIn(groupingPath);
-        window.location.reload();
+        setLoading(true);
+        try {
+            await optIn(groupingPath);
+        } catch (error) {
+            console.error('Error during opt-out:', error);
+        } finally {
+            setLoading(false);
+        }
     };
     return (
         <div className="flex justify-end items-center w-full h-full">
-                <Button className="w-[85px] h-[23px]" onClick={handleOptOut}>
+            <Button className="w-[85px] h-[23px]" onClick={handleOptOut}>
+                {loading ? (
+                    <div
+                        className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                        role="status"
+                    ></div>
+                ) : (
                     <FontAwesomeIcon icon={faUserPlus} />
-                </Button>
+                )}
+            </Button>
         </div>
     );
 };
--- ui/src/app/memberships/_components/memberships-optout-cell.tsx
+++ ui/src/app/memberships/_components/memberships-optout-cell.tsx
@@ -1,20 +1,35 @@
-import { Button } from '@/components/ui/button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserMinus } from '@fortawesome/free-solid-svg-icons';
-import { optOut } from '@/lib/actions';
+import {useState} from 'react';
+import {Button} from '@/components/ui/button';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faUserMinus} from '@fortawesome/free-solid-svg-icons';
+import {optOut} from '@/lib/actions';
 
-const MembershipsOptOutCell = ({ optOutEnabled, groupingPath }: { optOutEnabled: boolean; groupingPath: string }) => {
+const MembershipsOptOutCell = ({optOutEnabled, groupingPath}: { optOutEnabled: boolean; groupingPath: string }) => {
+    const [loading, setLoading] = useState(false);
+
     const handleOptOut = async () => {
-
-        await optOut(groupingPath);
-        window.location.reload();
-
+        setLoading(true);
+        try {
+            await optOut(groupingPath);
+        } catch (error) {
+            console.error('Error during opt-out:', error);
+        } finally {
+            setLoading(false);
+        }
     };
+
     return (
         <div className="flex justify-end items-center w-full h-full">
             {optOutEnabled ? (
-                <Button className="w-[85px] h-[23px]" onClick={handleOptOut}>
-                    <FontAwesomeIcon icon={faUserMinus} />
+                <Button className="w-[85px] h-[23px]" onClick={handleOptOut} disabled={loading}>
+                    {loading ? (
+                        <div
+                            className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                            role="status">
+                        </div>
+                    ) : (
+                        <FontAwesomeIcon icon={faUserMinus}/>
+                    )}
                 </Button>
             ) : (
                 <span className="text-text-color font-normal text-[1rem] pr-3">Required</span>
