eb5ccccd76d4e9b8aa6e343ab4518b9d499c66ec
Ralph Jhon Ramos <22ralph.ramos@gmail.com>
2024-11-08 16:11:12 -1000

    global-filter.tsx fix

--- ui/src/components/table/table-element/global-filter.tsx
+++ ui/src/components/table/table-element/global-filter.tsx
@@ -1,11 +1,3 @@
-/*import { Input } from '@/components/ui/input';
-import { Dispatch, SetStateAction } from 'react';
-
-const GlobalFilter = ({ filter, setFilter }: { filter: string; setFilter: Dispatch<SetStateAction<string>> }) => (
-    <Input placeholder="Filter Groupings..." value={filter || ''} onChange={(e) => setFilter(e.target.value)} />
-);
-
-export default GlobalFilter;*/
 
 import { Input } from '@/components/ui/input';
 import { Dispatch, SetStateAction } from 'react';
