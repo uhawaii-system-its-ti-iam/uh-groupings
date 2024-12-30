d5b016c5544f0cfa831622cc12e548e5e9f2c6a5
Prayag Das <prayag.j.das2001@gmail.com>
2024-12-30 14:08:47 -0800

    Finished question-item component. Continued working on image-item component.

---
+++ ui/src/app/about/_components/image-item.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+type ImageItem = {
+    description: string;
+    icon: {
+        src: string;
+        alt: string;
+    };
+};
+
+type ImageItemProps = {
+    data: ImageItem[];
+};
+
+const ImageItem = async ({ data }: ImageItemProps, { size }: { size?: 'lg' | 'default' }) => {
+    const textSize = size === 'lg' ? 'text-[1.2rem]' : 'text-base';
+
+    return (
+        <div className="grid gap-8">
+            {data.map((infoItem, index) => (
+                <div key={index} className="col-span-1">
+                    <div className="flex justify-center">
+                        <Image src={infoItem.icon.src} alt={infoItem.icon.alt} width={115} height={115} />
+                    </div>
+                    <p className={`text-center font-normal ${textSize}`}>{infoItem.description}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ImageItem;
