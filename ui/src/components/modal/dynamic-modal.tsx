7830d6971ba914df45667f2f7e4e008e0f0e9070
RuiChen12 <100800368+RuiChen12@users.noreply.github.com>
2025-02-11 15:49:46 -1000

    Begin implementing actions, preferences and sync destinations

--- ui/src/components/modal/dynamic-modal.tsx
+++ ui/src/components/modal/dynamic-modal.tsx
@@ -7,7 +7,7 @@ import {
     AlertDialogDescription,
     AlertDialogTitle,
     AlertDialogFooter,
-    AlertDialogCancel
+    AlertDialogCancel,
 } from '@/components/ui/alert-dialog';
 import { ReactNode } from 'react';
 import { Button } from '@/components/ui/button';
@@ -16,13 +16,17 @@ const DynamicModal = ({
     open,
     title,
     body,
+    warning,
     buttons,
-    onClose
+    closeText = 'Cancel',
+    onClose,
 }: {
     open: boolean;
     title: string;
     body: string;
+    warning?: string;
     buttons?: ReactNode[];
+    closeText?: string;
     onClose: () => void;
 }) => {
     return (
@@ -31,15 +35,25 @@ const DynamicModal = ({
                 <AlertDialogHeader>
                     <AlertDialogTitle>{title}</AlertDialogTitle>
                     <AlertDialogDescription>{body}</AlertDialogDescription>
+                    {warning && (
+                        <AlertDialogDescription>
+                            <div className="bg-yellow-100 border border-yellow-200 p-3 rounded-md ml-2.5">
+                                {warning}
+                            </div>
+                        </AlertDialogDescription>
+                    )}
                 </AlertDialogHeader>
                 <AlertDialogFooter>
-                    <AlertDialogCancel onClick={onClose}>OK</AlertDialogCancel>
-                    {/*Any buttons that should lead the user to a different page.*/}
                     {buttons?.map((button, index) => (
-                        <Button key={index} onClick={onClose}>
+                        <Button
+                            key={index}
+                            onClick={onClose}
+                            className="bg-uh-teal text-white hover:bg-uh-teal/90"
+                        >
                             {button}
                         </Button>
                     ))}
+                    <AlertDialogCancel onClick={onClose}>{closeText}</AlertDialogCancel>
                 </AlertDialogFooter>
             </AlertDialogContent>
         </AlertDialog>
